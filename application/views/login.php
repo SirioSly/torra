8fae84663887c8ac95214076f9458045f277c589
Home <Home@DESKTOP-GR9DHNT>
2018-10-31 16:04:58 -0300

    Ajustes Tela Login

--- application/views/login.php
+++ application/views/login.php
@@ -1,68 +1,52 @@
-<!DOCTYPE html>
-
 <div class="limiter">
-    
-<div class="container-login100">
-        <div class="wrap-login100">
-            <div class="login100-pic js-tilt" data-tilt>
-					<img src="images/principal.png" alt="IMG">
+		<div class="container-login100">
+			<div class="wrap-login100">
+                            <div class="login100-pic js-tilt" data-tilt>
+					<img src="images/imgteste.png" alt="IMG">
 				</div>
-    <?php echo form_open('login/autenticar'); ?> 
-        <span class="login100-form-title">
+                            <?php echo form_open('login/autenticar'); ?> 
+					<span class="login100-form-title">
 						Login
 					</span>
-    <center>
-    <div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
-<!--        <label for="nomeusuario">Nome Usuario:</label>-->
-        <input name="usuario" type="text" class="input100"  id="nomeusuario" placeholder="Usuario">
-        <span class="focus-input100"></span>
+
+					<div class="wrap-input100 validate-input" data-validate = "O usuario é exigido">
+						<input name="usuario" type="text" class="input100"  id="nomeusuario" placeholder="Usuario">
+						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
+							<i class="fa fa-user" aria-hidden="true"></i>
 						</span>
+					</div>
 
-    </div>
-        </center>
-
-    <center>
-    <div class="wrap-input100 validate-input m-b-16">
-<!--        <label for="senha">Senha:</label>-->
-        <input name="senha" type="password" class="input100"  id="senha" placeholder="Senha">
-        <span class="focus-input100"></span>
+					<div class="wrap-input100 validate-input" data-validate = "A senha é exigida">
+						<input name="senha" type="password" class="input100"  id="senha" placeholder="Senha">
+						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
+					</div>
 
-    </div>
-        
-    </center>
+					<div class="container-login100-form-btn">
+						<input class="login100-form-btn" type="submit" value="Entrar"/>
+					</div>
 
-
-    <p></p>
-
-        <div class="container-login100-form-btn">
-    <input class="login100-form-btn" type="submit" value="Entrar"/>
-        </div>
-    <br>
-    <input class="login100-form-btn" type="reset" value="Redefinir"/>
-    </br>
-    <br>
-    </br>
-    <br>
-    </br>
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Esqueceu
+						</span>
+						<a class="txt2" href="#">
+							Usuario / Senha?
+						</a>
+					</div>
     <?php form_close(); ?>
-        </div>
-    </div>
-    <p></p>
-    
-    <br>
-     </br>
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+<!--							Create your Account-->
+<!--							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>-->
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 
-
-
-
-
-<p></p>
-</div>
-
-</div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -41,6 +41,16 @@ and open the template in the editor.
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+
+
 
 
     </head>
