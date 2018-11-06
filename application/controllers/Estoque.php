f02e6ab4a3b7048ce83714c4a3d0f1b1a69516f6
Aluno <Aluno@GRVL301A-02>
2018-11-06 21:53:54 -0200

    estoque quase pronto

--- application/controllers/Estoque.php
+++ application/controllers/Estoque.php
@@ -6,83 +6,84 @@ class Estoque extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('Estoque_model', 'usuario');
-        if(!$this->session->userdata('estou_logado')){
-            redirect('login');
-        } else if($this->session->userdata('logado')->perfilAcesso == 'USER'){
-            redirect('home');
-        }
-        //contatos é um alias para o Contatos_model 
+        $this->load->model('Estoque_model', 'estoque');
+        $this->load->model('Fornecedor_model', 'fornecedor');
+//        if(!$this->session->userdata('estou_logado')){
+//            redirect('login');
+//        } else if($this->session->userdata('logado')->perfilAcesso == 'USER'){
+//            redirect('home');
+//        }
+//        contatos é um alias para o Contatos_model 
     }
-
     public function index() {
         $this->load->view('template/header');
         $dados['acronico'] = "LTT";
         $dados['completo'] = "Lojao Torra Torra";
-        $dados['usuario'] = $this->usuario->listar();
-        $this->db->join('fornecedor','fornecedor.idforce = estoque.idestoque');
-        $this->load->view('usuario', $dados);
+        $dados['estoque'] = $this->estoque->listar();
+//        $this->db->join('fornecedor','fornecedor.idforce = estoque.idestoque');
+        $this->load->view('estoque', $dados);
         $this->load->view('template/footer');
     }
 
-    public function inserir() {
+    public function inserir($e) {
         $data['nomeEstoque'] = mb_convert_case ($this->input->post('nomeEstoque'), MB_CASE_UPPER);
-        $data['qnt'] = mb_convert_case ($this->input->post('nomeEstoque'), MB_CASE_UPPER);
+        $data['qnt'] = mb_convert_case ($this->input->post('qntEstoque'), MB_CASE_UPPER);
         $data['valorVenda'] = mb_convert_case ($this->input->post('valorVenda'), MB_CASE_UPPER);
         $data['custo'] = mb_convert_case ($this->input->post('custo'), MB_CASE_UPPER);
+        $data['fornecedor'] = mb_convert_case ($this->input->post('custo'), MB_CASE_UPPER);
         $data['lote'] = mb_convert_case ($this->input->post('lote'), MB_CASE_UPPER);
         $data['validade'] = mb_convert_case ($this->input->post('validade'), MB_CASE_UPPER);
-        $data['user'] = mb_convert_case ($this->input->post('user'), MB_CASE_LOWER);
-        $data['senha'] = md5($this->input->post('senha'));
-        $data['perfilAcesso']= mb_convert_case($this->input->post('perfilAcesso'),MB_CASE_UPPER);
-        
-         $result = $this->usuario->inserir($data);
+        $data['produtocomposto'] = mb_convert_case ($this->input->post('validade'), MB_CASE_UPPER);
+        $data['status'] = mb_convert_case ($this->input->post('validade'), MB_CASE_UPPER);
+                                 
+         $result = $this->estoque->inserir($data);
         if ($result == TRUE){
             $this->session->set_flashdata('sucesso', 'msg');
-             redirect('usuario');
+             redirect('estoque');
         } else{
             $this->session->set_flashdata('falha', 'msg');
-            redirect('usuario');
+            redirect('estoque');
         }
         
        
     }
 
-    public function excluir($id) {
-        $result = $this->usuario->deletar($id);
+    public function excluir($idestoque) {
+        $result = $this->estoque->deletar($idestoque);
         if ($result == TRUE){
             $this->session->set_flashdata('excluirS', 'msg');
-             redirect('usuario');
+             redirect('estoque');
         } else{
             $this->session->set_flashdata('excluirF', 'msg');
-            redirect('usuario');
+            redirect('estoque');
         }
     }
 
-    function editar($id) {
+    function editar($idestoque) {
         $this->load->view('template/header');
-        $data['acronico'] = "MPF";
-        $data['completo'] = "Meu Projeto Framework";
-        $data['usuarioEditar'] = $this->usuario->editar($id);
-        $this->load->view('usuarioEditar', $data);
+        $dados['acronico'] = "LTT";
+        $dados['completo'] = "Lojao Torra Torra";
+        $data['estoqueEditar'] = $this->estoque->editar($idestoque);
+        $this->load->view('estoqueEditar', $data);
         $this->load->view('template/footer');
-        
-    }
+              
+        }
 
-   public function atualizar(){
-        $data['idusuario'] = $this->input->post('idusuario');
-        $data['nomeUsuario'] = mb_convert_case ($this->input->post('nomeUsuario'), MB_CASE_UPPER);
-        $data['user'] = mb_convert_case ($this->input->post('user'), MB_CASE_LOWER);
-        $data['senha'] = md5($this->input->post('senha'));
-        $data['perfilAcesso']= mb_convert_case($this->input->post('perfilAcesso'),MB_CASE_UPPER);
-        
-        $result = $this->usuario->atualizar($data);
+   public function atualizar($data){
+        $data['idestoque'] = $this->input->post('idestoque');            
+        $data['nomeEstoque'] = mb_convert_case ($this->input->post('nomeEstoque'), MB_CASE_UPPER);
+        $data['qnt'] = mb_convert_case ($this->input->post('qntEstoque'), MB_CASE_UPPER);
+        $data['valorVenda'] = mb_convert_case ($this->input->post('valorVenda'), MB_CASE_UPPER);
+        $data['custo'] = mb_convert_case ($this->input->post('custo'), MB_CASE_UPPER);
+        $data['lote'] = mb_convert_case ($this->input->post('lote'), MB_CASE_UPPER);
+        $data['validade'] = mb_convert_case ($this->input->post('validade'), MB_CASE_UPPER);          
+        $result = $this->estoque->atualizar($data);
         if ($result == TRUE){
-            $this->session->set_flashdata('sucessoA', 'msg');
-             redirect('usuario');
+            $this->estoque->set_flashdata('sucessoA', 'msg');
+             redirect('estoque');
         } else{
-            $this->session->set_flashdata('falhaA', 'msg');
-            redirect('usuario');
+            $this->estoque->set_flashdata('falhaA', 'msg');
+            redirect('estoque');
         }
     }
   }
--- application/models/Estoque_model.php
+++ application/models/Estoque_model.php
@@ -6,13 +6,14 @@ class Estoque_model extends CI_Model{
     function __construct(){
         parent::__construct();
     }
+  
     
     function listar(){
         $this->db->select('*');
         $this->db->from('estoque');
         $this->db->order_by('nomeEstoque','ASC');
-        $this->db->join('fornecedor','fornecedor.idforce = estoque.idestoque');
-        $this->db->order_by('nome','ASC');
+        $this->db->join('fornecedor','fornecedor.idforce = estoque.idforce');
+        $this->db->order_by('nomeEstoque','ASC');
         $query=$this->db->get();
     return $query->result();
     }    
