2b934b5a85ab1014440c712b24f46ee512904094
Aluno <Aluno@GRVL301A-15>
2018-11-14 18:49:14 -0300

    forma de pagamento CRUD completo e funcional

--- application/views/meioPagEditar.php
+++ application/views/meioPagEditar.php
@@ -7,10 +7,10 @@
         <div class="col-xs-10 col-sm-10 col-lg-6">
             <?php echo form_open('meioPag/atualizar'); ?>
             <div class="form-group">
+                
                 <label for="nomemeioPag">Cadastrar Forma de Pagamento</label>
                 <input type="hidden" name="idmeioPag" id="idmeioPag" value="<?php echo $meioPagEditar[0]->idmeioPag; ?>">
-                <input name="nomemeioPag" type="text" class="col-sm-3 col-form-label form-control"  id="nomemeioPag">
-                <br>
+                <input name="nomemeioPag" type="text" class="col-sm-3 col-form-label form-control"  id="nomemeioPag" value="<?php echo $meioPagEditar[0]->nomemeioPag; ?>"/>                <br>
                 <input class="btn btn-success" type="submit" value="Salvar"/>
                 <input class="btn btn-secondary" type="reset" value="Limpar"/>
                 <?php form_close(); ?>
